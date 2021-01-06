99c516fc53f7b7ed2c490a49e4bb9a69c0bc5eed
clerysemanaz <clery.sem@gmail.com>
2021-01-06 10:44:09 +0100

    Step2 part I

---
+++ code/test/putchar.c
@@ -0,0 +1,26 @@
+/* putchar.c
+ *	Simple program to test whether writing with putchar works.
+ *	
+ *	Just do a "syscall" that write char on standard output.
+ *
+ */
+
+#include "syscall.h"
+
+void print(char c, int n)
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        //PutChar(c+i);
+    }
+    //PutChar('\n');
+}
+
+int main ()
+{
+    print('a', 4);
+    Halt ();
+
+    /* not reached */
+    return 0;
+}
