1f6b4bccfe037d87716f4cf7e24b10faab165f0d
Jah003 <jahna.florine@gmail.com>
2021-01-06 11:35:53 +0100

    step2_part2

--- code/userprog/progtest.cc
+++ code/userprog/progtest.cc
@@ -86,9 +86,14 @@ ConsoleTest (char *in, char *out)
     for (;;)
       {
 	  readAvail->P ();	// wait for character to arrive
-	  ch = console->GetChar ();
+	  ch = console->GetChar ();      
+      if (ch == EOF) { return; }
+      console->PutChar ('<');    // echo it!
+      writeDone->P ();  // wait for write to finish
 	  console->PutChar (ch);	// echo it!
 	  writeDone->P ();	// wait for write to finish
+      console->PutChar ('>');    // echo it!
+      writeDone->P ();  // wait for write to finish
 	  if (ch == 'q')
 	      return;		// if q, quit
       }
