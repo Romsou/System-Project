74780767041a147150e5b26a91fb8f2807f19571
Jah003 <jahna.florine@gmail.com>
2021-01-06 14:21:00 +0100

    SynchConsole : constructeur et SynchGetChar, compilation réussie

--- code/userprog/synchconsole.cc
+++ code/userprog/synchconsole.cc
@@ -26,18 +26,7 @@ SynchConsole::SynchConsole(char *readFile, char *writeFile)
 	readAvail = new Semaphore("read avail", 0);
 	writeDone = new Semaphore("write done", 0);
 
-	int in,out;
-
-	if (readFile == NULL)
-		in = 0;					// keyboard = stdin
-    else
-    	in = OpenForReadWrite(readFile, TRUE);	// should be read-only
-    if (writeFile == NULL)
-		out = 1;				// display = stdout
-    else
-    	out = OpenForWrite(writeFile);
-
-	console = new Console (in, out, ReadAvail, WriteDone, 0);
+	console = new Console (readFile, writeFile, ReadAvail, WriteDone, 0);
 }
 SynchConsole::~SynchConsole()
 {
