aaddb512e38b3de138accb0229243f877f92416a
Jah003 <jahna.florine@gmail.com>
2021-01-08 16:02:05 +0100

    avancee UserThreadCreate

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -6,6 +6,7 @@ do_UserThreadCreate(int f,int arg)
 {
 	//preparer environment 
   Thread *newThread = new Thread("new_user_thread");
+  newThread->space = currentThread->space;
   newThread->Fork(StartUserThread,f);
   return arg;
 }
