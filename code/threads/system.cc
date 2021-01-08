e3adae3f52f69227282ccc97d68a67365f1b0318
Romsou <ro.soumard@laposte.net>
2021-01-08 14:57:52 +0100

    Fix sytem.cc and exception.cc

--- code/threads/system.cc
+++ code/threads/system.cc
@@ -83,10 +83,10 @@ void Initialize(int argc, char **argv)
     int argCount;
     const char *debugArgs = "";
     bool randomYield = FALSE;
-    char* inputFile = NULL;
-    char* outputFile = NULL;
 #ifdef USER_PROGRAM
     bool debugUserProg = FALSE; // single step user program
+    char* inputFile = NULL;
+    char* outputFile = NULL;
 #endif
 #ifdef FILESYS_NEEDED
     bool format = FALSE; // format disk
