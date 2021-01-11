4b6117edcf451f68e1b733e1e50ba0cc3df89dbc
Romsou <ro.soumard@laposte.net>
2021-01-11 16:23:16 +0100

    StartUserThread finally done

--- code/test/userthreadcreateV2.c
+++ code/test/userthreadcreateV2.c
@@ -1,14 +1,15 @@
 #include "syscall.h"
 
-static void print() 
+static void print(void *arg)
 {
-   PutString("Baba");
+    char* phrase = (char*) arg;
+    PutString(phrase);
 }
 
-int main() 
+int main()
 {
-    int ret = UserThreadCreate(print, 0);
-    //PutString("Threadmain");
-    //UserThreadExit(print);
+    char* phrase = "hello coco\n";
+    int ret = UserThreadCreate(print, phrase);
+
     return ret;
 }
--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -3,12 +3,7 @@
 #include "thread.h"
 #include "system.h"
 #include "machine.h"
-
-struct forkArgs
-{
-  int func;
-  int args;
-};
+#include "userthread.h"
 
 /**
  * Starts a new user thread with function f
@@ -18,26 +13,35 @@ struct forkArgs
  * initialize the PC register with the address of f so that we can
  * jump to the user code once it is called.
  * 
- * @param f: The address of the function we want to jump to
+ * @param f: The address of a structure containing the function and the args
+ *           we want to jump to.
  */
 static void StartUserThread(int f)
 {
-  int stackAddress = machine->ReadRegister(StackReg);
-
-  machine->WriteRegister(PCReg, ((forkArgs *)f)->func);
+  // Used to put the StackReg at the end of the address space
+  currentThread->space->InitRegisters();
+  
+  int stackaddress = machine->ReadRegister(StackReg) + 16;
+  
+  machine->WriteRegister(PCReg, ((FunctionAndArgs *)f)->func);
   machine->WriteRegister(NextPCReg, machine->ReadRegister(PCReg) + 4);
-  machine->WriteRegister(StackReg, stackAddress + 2 * PageSize);
+  machine->WriteRegister(StackReg, stackaddress - 2 * PageSize);
+  machine->WriteRegister(4, ((FunctionAndArgs *)f)->args);
 
   machine->Run();
 }
 
 /**
- * do_UserThreadCreate
+ * Create a new user thread and puts it in the ready list.
+ * 
+ * @param f: The function we want to create a user thread for.
+ * @param arg: The argument we want to pass to f
+ * @return: 0 or -1 if the creation of the thread fails 
  */
 int do_UserThreadCreate(int f, int arg)
 {
 
-  struct forkArgs *fArgs = (forkArgs *)malloc(sizeof(forkArgs));
+  struct FunctionAndArgs *fArgs = (FunctionAndArgs *)malloc(sizeof(FunctionAndArgs));
   fArgs->args = arg;
   fArgs->func = f;
 
@@ -47,7 +51,8 @@ int do_UserThreadCreate(int f, int arg)
   if (newThread == NULL)
     return -1;
 
-  currentThread->Yield();
+  for(;;)
+    currentThread->Yield();
 
   return 0; //Return something about the thread... tid?
 }
