725ac5b3fa1c49d236d7b59fb76ec1ae4d33bf19
clerysemanaz <clery.sem@gmail.com>
2021-01-16 12:01:17 +0100

    fix test manythreads

--- code/test/userthreadcreate_manythreads.c
+++ code/test/userthreadcreate_manythreads.c
@@ -2,25 +2,17 @@
 
 #define NB_THREADS 100
 
-int t1;
-
 static void print(void *arg)
 {
     char* phrase = (char*) arg;
     PutString(phrase);
-    
-    if(phrase[0] != 'b') {
-      //PutString("aaaaaaaaaaaaaaaa");
-      UserThreadJoin(t1);
-    }
 }
 
 int main()
 {
     char* phrase = "Un thread\n";
-    char* phrase2 = "bb";
-    int i, nbThreads=1, nbThreadsError=0;
-    t1 = UserThreadCreate(print, phrase2);
+    
+    int i, nbThreads=0, nbThreadsError=0;
     for (i=0; i<NB_THREADS; i++) {
       if (UserThreadCreate(print, phrase) != -1) {
         nbThreads++;
@@ -29,12 +21,12 @@ int main()
       }
 
       //Only in order to slow down main thread
-      // int a =1 + 6 * 8 - 2 / 7 + 6 / 7 - 9;
-      // int b = 9 * 7 / 2 - 6 + 3 + 8 + 5;
-      // b = a * 8 - 999 + 6 / 3;
-      // a = b + a - 88 / 2 * 3;
-      // a = 999 * b - 88 / 7;
-      // b = a / 7;
+      int a =1 + 6 * 8 - 2 / 7 + 6 / 7 - 9;
+      int b = 9 * 7 / 2 - 6 + 3 + 8 + 5;
+      b = a * 8 - 999 + 6 / 3;
+      a = b + a - 88 / 2 * 3;
+      a = 999 * b - 88 / 7;
+      b = a / 7;
     }
 
     PutString("NbThreads : "); 
