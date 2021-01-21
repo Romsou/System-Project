f13a8b73425ffba6897273681d76056938f1bd44
Romsou <ro.soumard@laposte.net>
2021-01-21 11:21:04 +0100

    quick fix

--- code/test/test_userthreads.sh
+++ code/test/test_userthreads.sh
@@ -8,7 +8,7 @@ execute_programs() {
     for program in $(ls | grep -i ^userthread | cut -d "." -f1 | sort | uniq); do
         echo
         echo -e "==== \e[32mExecuting $program\e[0m ====\n"
-        ./${nachos} -x ${program}
+        ./${nachos} -rs 1 -x ${program}
         echo "Press any key to start next program"
         read
     done
