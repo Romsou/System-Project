f38f42d31e97548138f58f9476b828537a8e4112
Jah003 <jahna.florine@gmail.com>
2021-01-25 15:51:51 +0100

    aout test filesys

--- code/test/filesys_create.c
+++ code/test/filesys_create.c
@@ -3,6 +3,16 @@
 int main ()
 {
     Create("f1");
+    int fd = Open("f1");
+    char *s = "Blablabla";
+    char *res = "";
+    int len = 10;
+
+    Write(s,len,fd);
+    Read(res,len,fd);
+    PutString(res);
+    Close(fd);
+
     End();
     /* not reached */
     return 0;
