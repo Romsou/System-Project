f795cd8b39f6993dba0ff22ab623506f8d3d345d
Romsou <ro.soumard@laposte.net>
2021-01-27 18:00:16 +0100

    Testing script for final step

---
+++ code/test/test_final.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+exec=nachos-step6
+old_wd=$(pwd)
+green="\e[32m"
+red="\e[31m"
+reset="\e[0m"
+checkmark="\xE2\x9C\x94"
+
+gecho() {
+    echo -e ${green}$*${reset}
+}
+
+redecho() {
+    echo -e ${red}$*${reset}
+}
+
+cd ../build
+
+echo -n "Compilation."
+make clean > /dev/null && make > /dev/null
+echo -n "."
+[ ! -f $exec ] && echo "Erreur de compilation"
+echo -n ". "
+gecho "\xE2\x9C\x94"
+
+gecho "Formattage du disque dur..."
+./$exec -f > /dev/null
+echo 
+
+gecho "Copie de tous les fichiers dans le système..."
+for i in $(ls|grep -i ^user|grep -v .o$); do
+    echo -n "Copie de $i..." 
+    ./$exec -cp $i $i > /dev/null
+    gecho " $checkmark"
+done
+
+gecho "Test des exécutable..."
+for i in $(ls|grep -i ^user|grep -v .o$); do
+    echo -n "Lancement de "
+    gecho $i
+    ./$exec -x $i $i
+    echo 
+done
+
+echo "Test réseau simple..."
+cd $old_pwd
+./test_network_machine_ring.sh
+
+exit 0
