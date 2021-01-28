30c7365ce3fcf9a8f05a64b7611fa5b54952397d
clerysemanaz <clery.sem@gmail.com>
2021-01-28 11:22:14 +0100

    better tests

--- code/test/process_forkexec.c
+++ code/test/process_forkexec.c
@@ -1,11 +1,23 @@
 #include "syscall.h"
 
+void print(void *arg){
+  char* phrase = (char*) arg;
+  PutString(phrase);
+}
+
 int main() {
   char* file1 = "userpages0";
   char* file2 = "userpages1";
   ForkExec(file1);
   ForkExec(file2);
 
-  End();
+  char* phrase = "Je suis la phrase\n";
+  PutString("Création des userThread\n");
+  UserThreadCreate(print, phrase);
+  UserThreadCreate(print, phrase);
+  UserThreadCreate(print, phrase);
+  PutString("Fin du test\n");
+
+  // End();
   return 0;
 }
